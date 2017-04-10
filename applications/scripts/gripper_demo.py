51dff39e7e1387b596b39600bc0e6b838ed1c4d0
Justin Huang <jstn@cs.washington.edu>
2017-04-09 19:05:32 -0700

    Implemented Gripper.

--- applications/scripts/gripper_demo.py
+++ applications/scripts/gripper_demo.py
@@ -31,9 +31,10 @@ def main():
         effort = float(argv[2])
 
     if command == 'open':
-        rospy.logerr('Not implemented.')
+        gripper.open()
     elif command == 'close':
-        rospy.logerr('Not implemented.')
+        rospy.loginfo(effort)
+        gripper.close(effort)
     else:
         print_usage()
 
