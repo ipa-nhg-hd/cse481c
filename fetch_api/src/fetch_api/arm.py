8c76a11406ab6d851ae28777bfc4c13bfd4584f7
Justin Huang <jstn@cs.washington.edu>
2017-04-10 19:02:56 -0700

    Split Arm and ArmJoints

--- fetch_api/src/fetch_api/arm.py
+++ fetch_api/src/fetch_api/arm.py
@@ -1,6 +1,6 @@
-#!/usr/bin/env python
-
-import math
+# TODO: import ?????????
+# TODO: import ???????_msgs.msg
+# TODO: import ??????????_msgs.msg
 import rospy
 
 from .arm_joints import ArmJoints
@@ -10,14 +10,32 @@ class Arm(object):
     """Arm controls the robot's arm.
 
     Joint space control:
-        joint_state = JointState()
+        joints = ArmJoints()
         # Fill out joint states
         arm = fetch_api.Arm()
-        arm.move_to_joints(joint_state)
+        arm.move_to_joints(joints)
     """
 
     def __init__(self):
+        # TODO: Create actionlib client
+        # TODO: Wait for server
         pass
 
-    def move_to_joints(self, joint_state):
+    def move_to_joints(self, arm_joints):
+        """Moves the robot's arm to the given joints.
+
+        Args:
+            arm_joints: An ArmJoints object that specifies the joint values for
+                the arm.
+        """
+        # TODO: Create a trajectory point
+        # TODO: Set position of trajectory point
+        # TODO: Set time of trajectory point
+
+        # TODO: Create goal
+        # TODO: Add joint name to list
+        # TODO: Add the trajectory point created above to trajectory
+
+        # TODO: Send goal
+        # TODO: Wait for result
         rospy.logerr('Not implemented.')
