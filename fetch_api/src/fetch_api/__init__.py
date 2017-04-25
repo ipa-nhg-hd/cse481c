7cf98a4f325582239e89a08e096a01a1c9b2b41b
Justin Huang <jstn@cs.washington.edu>
2017-04-25 04:14:25 -0700

    Added MoveItGoalBuilder.

--- fetch_api/src/fetch_api/__init__.py
+++ fetch_api/src/fetch_api/__init__.py
@@ -2,5 +2,6 @@ from .arm import Arm
 from .arm_joints import ArmJoints
 from .base import Base
 from .gripper import Gripper
+from .moveit_goal_builder import MoveItGoalBuilder
 from .torso import Torso
 from .moveit_goal_builder import MoveItGoalBuilder
