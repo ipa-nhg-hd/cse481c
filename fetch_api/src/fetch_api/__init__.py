0384a51e7598f2b956e43e57e37a10c35956001a
Justin Huang <jstn@cs.washington.edu>
2017-04-26 20:06:30 -0700

    Update __init__.py

--- fetch_api/src/fetch_api/__init__.py
+++ fetch_api/src/fetch_api/__init__.py
@@ -2,6 +2,7 @@ from .arm import Arm
 from .arm_joints import ArmJoints
 from .base import Base
 from .gripper import Gripper
+from .gripper_viz import gripper_interactive_marker
 from .moveit_goal_builder import MoveItGoalBuilder
 from .torso import Torso
 from .moveit_goal_builder import MoveItGoalBuilder
